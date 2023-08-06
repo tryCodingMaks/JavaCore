3ea3b807e62966f813b1fe921ba0e11cd1e950bc
tryCodingMaks <learnjavamaks@gmail.com>
2023-08-06 11:36:51 +0300

    version v1.0

---
+++ src/main/java/org/example/Calc/Calc.java
@@ -0,0 +1,19 @@
+package org.example.Calc;
+
+import java.util.Scanner;
+
+
+public class Calc {
+    /**
+     sum();      //Метод суммирования чисел
+     multiply(); //Метод умножения чисел
+     diff();     //Метод вычитания чисел
+     divide();   //Метод деления чисел
+     */
+
+
+    public static void main(String[] args) {
+        CalcLogic calculator = new CalcLogic();
+        calculator.work();
+    }
+}
---
+++ src/main/java/org/example/Calc/CalcLogic.java
@@ -0,0 +1,84 @@
+package org.example.Calc;
+
+import java.util.Scanner;
+
+public class CalcLogic {
+     double a = 0;
+     double b = 0;
+     boolean exit = false;
+     private final Scanner scanner;
+
+    public CalcLogic() {
+        this.a = 0;
+        this.b = 0;
+        this.exit = false;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void work() {
+        choice();
+    }
+
+    private void choose() {
+        System.out.println("Введите первое число: ");
+        a = inputDouble();
+        System.out.println("Введите второе число: ");
+        b = inputDouble();
+    }
+
+    private boolean choice() {
+        while (!exit) {
+            System.out.println();
+            System.out.println("***Добро пожаловать в калькулятор! Следуй шагам и у тебя все получится!***");
+            System.out.println("Выберите операцию: ");
+            System.out.println("1. Сложение");
+            System.out.println("2. Умножение");
+            System.out.println("3. Вычитание");
+            System.out.println("4. Деление");
+            System.out.println("5. ВЫХОД");
+
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    choose();
+                    sum();
+                }
+                case 2 -> {
+                    choose();
+                    multiply();
+                }
+                case 3 -> {
+                    choose();
+                    diff();
+                }
+                case 4 -> {
+                    choose();
+                    divide();
+                }
+                case 5 -> exit = true;
+                default -> System.out.println("Неверная команда");
+            }
+        }
+        return false;
+    }
+    private void divide() {
+        System.out.println((a + " / " + b + " = " +(b != 0 ?(a / b):("Нелья делить на ноль!"))));
+    }
+
+    private void diff() {
+        System.out.println(a + " - " + b + " = " + (a - b));
+    }
+
+    private void multiply() {
+        System.out.println(a + " * " + b + " = " + (a * b));
+    }
+
+    private void sum() {
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+
+    private double inputDouble() {
+        return new Scanner(System.in).nextDouble();
+    }
+}
